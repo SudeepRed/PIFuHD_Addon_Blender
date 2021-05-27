8b432f377540c4a095169b730d11e18e4c089e29
expnoob <sudeep19313@iiitd.ac.in>
2021-05-27 12:37:55 +0530

    setup

---
+++ PiFuHDController.py
@@ -0,0 +1,11 @@
+import os
+#cd /content/pifuhd/sample_images
+def cwd_sample_images():
+    os.chdir('pifuhd/sample_images')
+    print(os.getcwd())
+#cd /content/lightweight-human-pose-estimation.pytorch/
+def cwd_lightWeightPoseEstimation():
+    os.chdir('lightweight-human-pose-estimation.pytorch/')
+    print(os.getcwd())
+print(os.getcwd())
+cwd_sample_images()
