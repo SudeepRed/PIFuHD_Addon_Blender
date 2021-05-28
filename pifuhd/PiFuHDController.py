28c96ec720a1b34d2c20890bb5acf78202ad1a44
expnoob <sudeep19313@iiitd.ac.in>
2021-05-29 03:31:04 +0530

    rearranged file structure

---
+++ pifuhd/PiFuHDController.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d998a94f912ff678daecfb8831a89f9cc3d6c3d68b20144d48999f5356a15442
+size 4499
---
+++ pifuhd/lhpe/makeRectForImage.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6bf51fd05e6e9bed3c09ee1f503293b58febba0be3bde131ce4a94dc7dfa38d
+size 3866
